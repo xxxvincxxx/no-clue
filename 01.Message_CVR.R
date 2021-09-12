41ddc107d4e081f205bb670113b0a434e6fa56a9
xxxvincxxx <vincenzo.grass@gmail.com>
2021-09-12 18:28:58 +0200

    add chron jobs

--- 01.Message_CVR.R
+++ 01.Message_CVR.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 df <- data.frame(stringsAsFactors=FALSE,
           type = c("A", "A", "B", "B"),
-      platform = c("iOS", "Android", "iOS", "Android"),
+      setting = c("platform_1", "platform_2", "platform_1", "platform_2"),
         clicks = c(174000, 526000, 540000, 160000),
    conversions = c(16200, 38400, 46800, 11000))
 
