2cfdf897f0cf896c9b4ce708e66cd893d330c647
xxxvincxxx <vincenzo.grass@gmail.com>
2021-11-08 20:49:51 +0100

    reshape prop test

---
+++ 02.Cumulative_CVR_SQL.R
@@ -0,0 +1,11 @@
+## Generate random sample 
+
+### Table users
+# 1. user_id - VARCHAR(40)
+# 2. first_seen - TIMESTAMP 
+
+
+### Table paid_subscription
+# 1. user_id - VARCHAR(40)
+# 2. event_time - TIMESTAMP 
+# 3. subscription_product_id - VARCHAR(40)
